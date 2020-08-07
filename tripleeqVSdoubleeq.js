c61b53c9b5b2338abc3b559b9e3f453393a25930
Shuvoskywalker <shuvokoiri89@gmail.com>
2020-08-07 16:28:53 +0600

    added

---
+++ tripleeqVSdoubleeq.js
@@ -0,0 +1,33 @@
+let num1=1
+let num2=-5
+if (num1=num2) {
+console.log("they are equal")    
+} else {
+    console.log("they are not equal")
+}
+
+let numm1=2
+let numm2="2"
+if (numm1==numm2) {
+console.log("they are equal")    
+} else {
+    console.log("they are not equal")
+}
+
+
+let Numm1=1//0//
+let Numm2=true//false//
+if (Numm1==Numm2) {
+console.log("they are equal")    
+} else {
+    console.log("they are not equal")
+}
+
+
+let nummm1=2
+let nummm2="2"
+if (nummm1===nummm2) {
+console.log("they are equal")    
+} else {
+    console.log("they are not equal")
+}
