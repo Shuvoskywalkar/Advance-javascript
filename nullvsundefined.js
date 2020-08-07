9a1f79aa797e35ba1d22383eebe419d8eca98b5c
Shuvoskywalker <shuvokoiri89@gmail.com>
2020-08-07 15:50:09 +0600

    nullvsundefined

---
+++ nullvsundefined.js
@@ -0,0 +1,14 @@
+let value=null
+console.log(value)
+let value2=undefined
+console.log(value2)
+function add(num1,num2) {
+     console.log(num1+num2)
+    return 
+}
+const result=add(8,)
+console.log(result)
+const array=[4,5,6,8,1,2,null]
+console.log(array[6])
+const object={nam:"tom hanks",boyos:64,kam:"acting"}
+console.log(object.outofobject)
