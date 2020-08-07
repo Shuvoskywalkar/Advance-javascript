0c2c9260f431302f59d6f37a5edff697a264c54a
Shuvoskywalker <shuvokoiri89@gmail.com>
2020-08-07 15:00:21 +0600

    truevsfalse

---
+++ truthfalse.js
@@ -0,0 +1,19 @@
+//falsy
+//0
+//""
+//false
+//null
+//naN
+//undefined
+//TRUTHY
+//" "
+//"0"
+//{}
+//[]
+
+const name=12
+if (name||name==0) {
+    console.log("this is true")
+} else {
+console.log('this is false')
+}
